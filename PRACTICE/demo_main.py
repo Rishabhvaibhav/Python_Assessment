b0f93d6393b0eba6a597e60c2a928cec9a099f95
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-08-19 17:50:09 +0530

    update

--- PRACTICE/demo_main.py
+++ PRACTICE/demo_main.py
@@ -1,5 +1,18 @@
-def fibo ( x ):
-    a = (x-1)
-    b = (x-0)
-    
-    
+def fibo(x):
+    a =1
+    b = 1
+    if x == 1 :
+        print("0")
+    elif x==2:
+        print("0","1")
+    else:
+        print("0",a,b,end = " ")
+        
+        for i in range(x-3):
+            final = a + b
+            b = a
+            a = final
+            
+            print(final,end=  " ")
+            
+fibo(12)
